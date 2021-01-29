b8faaed644241ac83b447a7b2ee89fb2e6896c2c
adam <adamforsberg94@gmail.com>
2021-01-29 13:17:38 +0100

    added task 1.5

--- p1.sh
+++ p1.sh
@@ -2,7 +2,7 @@
 #Generate a rsa key and save it as ca.key
 openssl genrsa -out ca.key 	
 
- # Generate a self signed x509-certificate. save as ca.crt
+# Generate a self signed x509-certificate. save as ca.crt
 openssl req -new -x509 -key ca.key -out ca.crt
 
 #2.
@@ -18,8 +18,10 @@ keytool -genkeypair -keyalg RSA -keysize 2048 -keystore clientkeystore
 keytool -certreq -keystore clientkeystore -storepass password -file clientkey.csr 
 
 #5.
+#Signs the request 'clientkey.csr' and signs with CA. Saves to 'clientkey.crt'. (A file called ca.srl is created which i think contains all issued serial numbers)
+openssl x509 -req -in clientkey.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out clientkey.crt
 
-
+#6. 
 
 
 
